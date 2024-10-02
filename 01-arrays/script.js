0a431d3168453fce3bfcda3bd7812b215bda016d
feffe1702 <fpm.asd@gmail.com>
2024-10-02 16:57:11 +0200

    complete default tasks

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -48,12 +48,17 @@ console.log(isFabioPresent);
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
+
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
+
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length !== 0;
+console.log(isTeachersEmpty);
