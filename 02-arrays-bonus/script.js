448fdda19ee14c63321b91080b6d56af4989135d
feffe1702 <fpm.asd@gmail.com>
2024-10-02 17:25:36 +0200

    complete bonus tasks

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -7,13 +7,31 @@ const teachers = [
   'Lewis',
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
+console.log(teachers);
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+console.log(reversedTeachers);
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+let longNames = teachers
+for (let i = 0; i < teachers.length; i++) {
+  if(teachers[i].length < 5){
+    longNames.splice(i , 1)
+  }  
+}
+
+console.log(longNames);
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const rimozione = teachers.indexOf("Ed");
+if(rimozione !== -1){
+  teachers.splice(rimozione , 1);
+}
+
+console.log(teachers);
